b0ce003b243b3b36f67a7e9cfc2d4fa3b7d278f2
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-01-16 04:19:05 +0700

    Edited slash command /prune => /clear

--- commands/general/clear.js
+++ commands/general/clear.js
@@ -2,9 +2,15 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('prune')
+		.setName('clear')
 		.setDescription('Prune up to 99 messages.')
-		.addIntegerOption(option => option.setName('amount').setDescription('Number of messages to prune')),
+		.addIntegerOption(option => option
+			.setName('amount')
+			.setDescription('Number of messages to prune')
+			.setMinValue(1)
+			.setMaxValue(99)
+			.setRequired(true)
+		),
 	async execute(interaction) {
 		const amount = interaction.options.getInteger('amount');
 
@@ -12,7 +18,7 @@ module.exports = {
 			return interaction.editreply({ content: 'You need to input a number between 1 and 99.', ephemeral: true });
 		}
 		await interaction.channel.bulkDelete(amount, true).catch(error => {
-			console.error(error);
+			console.log(error);
 			return interaction.editreply({ content: 'There was an error trying to prune messages in this channel!', ephemeral: true });
 		});
 
