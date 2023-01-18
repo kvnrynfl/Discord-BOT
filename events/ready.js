4068767803616ac8e916a0ac53878db5ba822408
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-01-18 15:58:51 +0700

    Added connection to database

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,19 @@
 const { Events } = require('discord.js');
+const { createConnection } = require('mysql');
+const config = require('../config.json');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		connection = createConnection(config.mysql);
+		connection.connect(error => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log('MySQL has been connected!')
+		});
+		
 		// client.user.setActivity("🤖 | /help")
 		client.user.setActivity("🤖 | Maintenance | The bot is currently under development")
 		console.log(`Ready! Logged in as ${client.user.tag}`);
