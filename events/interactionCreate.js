131ae44a0679f95ae2257e427fa47d7602073e31
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-01-23 22:21:08 +0700

    Added interaction modal submit

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,25 +4,32 @@ module.exports = {
 	name: Events.InteractionCreate,
 	once: false,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+		if (interaction.isChatInputCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
 
-		const command = interaction.client.commands.get(interaction.commandName);
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+	
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+				await interaction.editReply({
+					content: "❌ | An error occurred, please report it using ``/report bug`` so that it can be fixed immediately",
+					ephemeral: true,
+				});
+			}
+		} else if (interaction.isModalSubmit()) {
+			await interaction.reply({ content: 'Your submission was received successfully!' });
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
+			const textinput1 = interaction.fields.getTextInputValue('textinput1');
+			const textinput2 = interaction.fields.getTextInputValue('textinput2');
+			console.log({ textinput1, textinput2 });
 		}
 
-		try {
-			await interaction.deferReply();
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
-			await interaction.editReply({
-                content: "❌ | An error occurred, please report it using ``/report bug`` so that it can be fixed immediately",
-                ephemeral: true,
-            });
-		}
+		
 	},
 };
