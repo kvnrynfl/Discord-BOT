b4d20a36f734068e2d24ec1e1211eeb08cd17c4d
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-01-27 23:28:58 +0700

    Added interaction string select menu

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,9 +1,13 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
+const randomColor = require('randomcolor');
 
 module.exports = {
 	name: Events.InteractionCreate,
 	once: false,
 	async execute(interaction) {
+		let color = randomColor();
+		let EventsEmbed = new EmbedBuilder();
+		// console.log(interaction);
 		if (interaction.isChatInputCommand()) {
 			const command = interaction.client.commands.get(interaction.commandName);
 
@@ -17,7 +21,7 @@ module.exports = {
 			} catch (error) {
 				console.error(`Error executing ${interaction.commandName}`);
 				console.error(error);
-				await interaction.editReply({
+				await interaction.reply({
 					content: "❌ | An error occurred, please report it using ``/report bug`` so that it can be fixed immediately",
 					ephemeral: true,
 				});
@@ -28,8 +32,27 @@ module.exports = {
 			const textinput1 = interaction.fields.getTextInputValue('textinput1');
 			const textinput2 = interaction.fields.getTextInputValue('textinput2');
 			console.log({ textinput1, textinput2 });
+		} else if (interaction.isStringSelectMenu()) {
+			switch (interaction.customId) {
+				case 'MusicPlay':
+					// EventsEmbed
+					// 	.setColor(color)
+					// 	.setTitle(`**🤖 Logging Select Menu**`)
+					// 	.setDescription(
+					// 		`Guild : ${interaction.guildId}\n` +
+					// 		`Channel : <#${interaction.channelId}>\n` +
+					// 		`User : <@${interaction.user.id}>\n` +
+					// 		`Costom Id : ${interaction.customId}\n` +
+					// 		`Values String : ${interaction.values.toString()}`
+					// 	);
+					// interaction.reply({ embeds : [EventsEmbed], ephemeral : true });
+					// console.log(interaction);
+					break;
+			
+				default:
+					console.log(`Unknow custom id button select menu`)
+					break;
+			}
 		}
-
-		
 	},
 };
