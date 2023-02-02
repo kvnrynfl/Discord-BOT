1ff51893571c647f1f086efb049831fd52ecb152
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-02-02 15:17:53 +0700

    Added database connection and database handler

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 // Require the necessary classes
 const { Client, Collection, GatewayIntentBits, Partials} = require('discord.js');
 const { Player } = require('discord-player');
-const config = require('./config.json');
+const mysql = require('mysql');
+require('dotenv').config()
 
 // Require file handlers
 // const { handleLogs } = require("./handlers/handleLogs");
 const { loadEvents } = require("./handlers/eventHandler");
 const { loadCommands } = require("./handlers/commandHandler");
+const { loadDatabase } = require("./handlers/databaseHandler");
 
 // Create a new client instance
 const client = new Client({ 
 	intents: [
 		GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildModeration,
         GatewayIntentBits.GuildEmojisAndStickers,
         GatewayIntentBits.GuildIntegrations,
         GatewayIntentBits.GuildWebhooks,
         GatewayIntentBits.GuildInvites,
+		GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.GuildPresences,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMessageReactions,
@@ -27,7 +30,8 @@ const client = new Client({
         GatewayIntentBits.DirectMessageTyping,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildScheduledEvents,
-		GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.AutoModerationConfiguration,
+        GatewayIntentBits.AutoModerationExecution,
 	],
 	partials: [
         Partials.User,
@@ -49,11 +53,20 @@ client.player = new Player(client, {
         quality: "highestaudio",
         highWaterMark: 1 << 25
     }
-})
+});
+
+client.database = mysql.createPool({
+    connectionLimit : process.env.SQL_LIMIT,
+    host	        : process.env.SQL_HOST,
+    user	        : process.env.SQL_USER,
+    password        : process.env.SQL_PASSWORD,
+    database        : process.env.SQL_DATABASE
+});
 
 // Log in to Discord with your client's token
 // Loaded All file Handlers
-client.login(config.client.token).then(() => {
+client.login(process.env.CLIENT_TOKEN).then(() => {
 	loadEvents(client);
 	loadCommands(client);
+	loadDatabase(client);
 });
