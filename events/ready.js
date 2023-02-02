4b83247ec789b426a24874d22dbe604a67f9cd3b
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-02-02 15:24:05 +0700

    Update activity in event ready

--- events/ready.js
+++ events/ready.js
@@ -1,21 +1,11 @@
 const { Events } = require('discord.js');
-const { createConnection } = require('mysql');
-const config = require('../config.json');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		connection = createConnection(config.mysql);
-		connection.connect(error => {
-			if (error) {
-				return console.log(error);
-			}
-			console.log('MySQL has been connected!')
-		});
-		
-		// client.user.setActivity("🤖 | /help")
-		client.user.setActivity("🤖 | Maintenance | The bot is currently under development")
+		client.user.setActivity("🤖 | /help")
+		// client.user.setActivity("🤖 | Maintenance | The bot is currently under development")
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
