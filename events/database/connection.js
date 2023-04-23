6a3011e13e4701f9269be2335a4526e694cb7f4d
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-04-23 11:33:05 +0700

    Remove Sql Event

--- events/database/connection.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'connection',
-	once: false,
-	async execute(connection) {
-		connection.query('SET SESSION auto_increment_increment=1');
-	},
-};
--- events/database/enqueue.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'enqueue',
-	once: false,
-	async execute(connection) {
-		console.log('Waiting for available connection slot');
-	},
-};
