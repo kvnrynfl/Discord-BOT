76e33e7807afff1920003a0607664713faff85bd
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-04-24 12:08:33 +0700

    Update command report bug

--- commands/general/report.js
+++ commands/general/report.js
@@ -1,21 +1,20 @@
-const { SlashCommandBuilder, PermissionFlagsBits, ModalBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle, EmbedBuilder} = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, ModalBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle, EmbedBuilder } = require('discord.js');
 const randomColor = require('randomcolor');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('report')
-		.setDescription('🤖 | Report bugs, players and more')
+    data: new SlashCommandBuilder()
+        .setName('report')
+        .setDescription('🤖 | Report bugs, players and more')
         .addSubcommand((subcommand) => subcommand
             .setName('bug')
             .setDescription('🤖 | Report bugs about bots')
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
         .setDMPermission(false),
-	async execute(interaction) {
-        const subcmd = interaction.options.getSubcommand(["bug", "player"]);
+    async execute(interaction) {
+        const subcmd = interaction.options.getSubcommand([ "bug", "player" ]);
         var color = randomColor();
         let NewEmbed = new EmbedBuilder();
-
 
         switch (subcmd) {
             case "bug":
@@ -24,28 +23,42 @@ module.exports = {
                         .setCustomId('InputGeneralReportBug1')
                         .setLabel("Full Name")
                         .setStyle(TextInputStyle.Short)
-                        .setPlaceholder('Please input your full name.')
+                        .setPlaceholder('Please enter your full name.')
                 );
                 const ARBReportBug2 = new ActionRowBuilder().addComponents(
                     new TextInputBuilder()
                         .setCustomId('InputGeneralReportBug2')
-                        .setLabel("What Happened")
-                        .setStyle(TextInputStyle.Paragraph)
-                        .setPlaceholder("Please explain about the bug that occurred.")
-                        .setValue("Title : \nScreenshot URL : \nDescription : ")
+                        .setLabel("Title")
+                        .setStyle(TextInputStyle.Short)
+                        .setPlaceholder("Please enter a brief title for the bug report.")
                 );
+                const ARBReportBug3 = new ActionRowBuilder().addComponents(
+                    new TextInputBuilder()
+                        .setCustomId('InputGeneralReportBug3')
+                        .setLabel("Screenshot URL")
+                        .setStyle(TextInputStyle.Short)
+                        .setPlaceholder("Please enter the URL of a screenshot (if applicable).")
+                );
+                const ARBReportBug4 = new ActionRowBuilder().addComponents(
+                    new TextInputBuilder()
+                        .setCustomId("InputGeneralReportBug4")
+                        .setLabel("Description")
+                        .setStyle(TextInputStyle.Paragraph)
+                        .setPlaceholder("Please describe the bug in detail, including the steps to reproduce it.")
+                );
+
                 const reportBug = new ModalBuilder()
                     .setCustomId('GeneralReportBug')
                     .setTitle('Bug Report Submission Form')
-                    .addComponents(ARBReportBug1, ARBReportBug2);
+                    .addComponents(ARBReportBug1, ARBReportBug2, ARBReportBug3, ARBReportBug4);
                 interaction.showModal(reportBug);
                 break;
             default:
                 NewEmbed
                     .setColor(color)
-                    .setDescription(`**❌ | Unknown sub command.**`)
-                interaction.reply({ embeds : [NewEmbed] });
+                    .setDescription(`**❌ | Invalid sub-command. Please try again.**`)
+                interaction.reply({ embeds: [ NewEmbed ] });
                 break;
-        }  
-	},
+        }
+    },
 };
