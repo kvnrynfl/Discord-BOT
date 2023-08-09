6c5a2b67d77fc3b6146906f2059b7fece7f32667
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-08-09 13:41:56 +0700

    Add playlist model

--- models/database.js
+++ models/database.js
@@ -2,14 +2,17 @@ const { model } = require("mongoose");
 
 const { userSchema } = require('./schemas/user');
 const { guildSchema } = require('./schemas/guild');
+const { playlistSchema } = require('./schemas/playlist');
 const { reportBugSchema } = require('./schemas/reportBug');
 
 const users = model("users", userSchema);
 const guilds = model("guilds", guildSchema);
+const playlists = model("playlists", playlistSchema);
 const reportbugs = model("reportbugs", reportBugSchema);
 
 module.exports = {
     users,
     guilds,
+    playlists,
     reportbugs,
 };
